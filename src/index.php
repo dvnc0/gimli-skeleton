5b15f9e761e2a639696cbbc7f556d6cd1e177b63
dvnc0 <dcobb.dev@gmail.com>
2025-04-16 01:10:24 -0400

    Update index.php format

--- src/index.php
+++ src/index.php
@@ -2,16 +2,19 @@
 declare(strict_types=1);
 require_once __DIR__ . '/vendor/autoload.php';
 
+use App\Core\Cache;
 use Gimli\Application;
 use App\Core\Config;
-use App\Core\Cache;
+use function Gimli\Injector\resolve_fresh;
 
 define('APP_ROOT', __DIR__);
 
-$App = Application::create(APP_ROOT, $_SERVER);
-$App->Config = $App->Injector->resolveFresh(Config::class, ['config' => parse_ini_file(APP_ROOT . '/App/Core/config.ini', true)]);
+$App = Application::create(APP_ROOT, $_SERVER)
+	->setConfig(resolve_fresh(Config::class, ['config' => parse_ini_file(APP_ROOT . '/App/Core/config.ini', true)]))
+	->enableLatte();
+
 if (class_exists('Redis')) {
-	$App->Injector->register(Cache::class, Cache::getCache($App->Config->cache));
+	$App->Injector->bind(Cache::class, Cache::getCache($App->Config->cache));
 }
 
 $App->run();
