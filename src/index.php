7e5daa17dbf759719fce1e1e31e3bf41733bb69b
dvnc0 <dcobb.dev@gmail.com>
2025-06-13 01:52:47 -0400

    updating for gimli changes

--- src/index.php
+++ src/index.php
@@ -5,16 +5,17 @@ require_once __DIR__ . '/vendor/autoload.php';
 use App\Core\Cache;
 use Gimli\Application;
 use App\Core\Config;
+use Gimli\Application_Registry;
+
 use function Gimli\Injector\resolve_fresh;
 
 define('APP_ROOT', __DIR__);
 
-$App = Application::create(APP_ROOT, $_SERVER)
-	->setConfig(resolve_fresh(Config::class, ['config' => parse_ini_file(APP_ROOT . '/App/Core/config.ini', true)]))
+$App = Application::create(APP_ROOT, $_SERVER);
+$App->setConfig(resolve_fresh(Config::class, ['config' => parse_ini_file(APP_ROOT . '/App/Core/config.ini', true)], $App))
 	->enableLatte();
 
-if (class_exists('Redis')) {
-	$App->Injector->bind(Cache::class, Cache::getCache($App->Config->cache));
-}
+$App->Injector->bind(Cache::class, fn() => new Cache(['host' => APP_ROOT . '/App/tmp/cache.sqlite']));
 
+Application_Registry::set($App);
 $App->run();
